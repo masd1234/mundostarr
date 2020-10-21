5b3b7551852a3beb989ab93b428e00a24b896af1
myusername <myemail>
2020-10-21 19:43:40 -0300

    Updates

---
+++ precache-manifest.ac144a321e56b375a60cf52bc0da19b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333e311a877055dcf12a5227c4281957",
+    "url": "/mundostarr/index.html"
+  },
+  {
+    "revision": "b77403758ac573a7e742",
+    "url": "/mundostarr/static/css/main.bbe0cab9.chunk.css"
+  },
+  {
+    "revision": "76d0f87dabe097727382",
+    "url": "/mundostarr/static/js/2.7c488dcd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mundostarr/static/js/2.7c488dcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b77403758ac573a7e742",
+    "url": "/mundostarr/static/js/main.d7d289c5.chunk.js"
+  },
+  {
+    "revision": "babbc7d230d0fc1379ca",
+    "url": "/mundostarr/static/js/runtime-main.0f894a63.js"
+  },
+  {
+    "revision": "189444cae067a5cff19d834f0375a1db",
+    "url": "/mundostarr/static/media/Aguada.189444ca.jpg"
+  },
+  {
+    "revision": "ba71e07c8bb876832ad673cdbefa8a38",
+    "url": "/mundostarr/static/media/office.ba71e07c.jpg"
+  },
+  {
+    "revision": "b5b4f006d0ac308b7c3d655c32063aba",
+    "url": "/mundostarr/static/media/user.b5b4f006.jpg"
+  }
+]);
